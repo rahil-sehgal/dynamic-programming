ee93a6b01b9e9e1526564150d887b9f46daa56fc
RAHIL SEHGAL <57448644+rahil-sehgal@users.noreply.github.com>
2020-08-19 18:08:56 +0530

    easy one same as previous prob

---
+++ Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        string a=s;
+        reverse(a.begin(),a.end());
+        
+        return  a.size()-LCS(a,s);
+    }
+    int LCS(string a,string b){
+        int m=a.size(),n=b.size();
+        int t[m+1][n+1];
+        
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0||j==0){
+                    t[i][j]=0;
+                }
+                else if(a[i-1]==b[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else t[i][j]= max(t[i-1][j],t[i][j-1]);
+                
+            }
+        }
+        return t[m][n];      
+    }
+    int max(int a,int b){
+        return (a>b)?a:b;}
+    
+};
